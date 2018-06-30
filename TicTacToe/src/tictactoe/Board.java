f5f522257cfaab591f84be1311b05c44a1896e8d
shivprogrammer <shiv.unadkat@gmail.com>
2018-06-30 13:01:06 -0700

    hasWon method added to Board class

--- TicTacToe/src/tictactoe/Board.java
+++ TicTacToe/src/tictactoe/Board.java
@@ -41,5 +41,23 @@ public class Board {
                 }
             }
         }
+        return true;
+    }
+    
+    public boolean hasWon (Seed theSeed) {
+        return (cells[currentRow][0].content == theSeed
+                        && cells[currentRow][1].content == theSeed
+                        && cells[currentRow][2].content == theSeed
+                || cells[0][currentCol].content == theSeed
+                        && cells[1][currentCol].content == theSeed
+                        && cells[2][currentCol].content == theSeed
+                || currentRow == currentCol
+                        && cells[0][0].content == theSeed
+                        && cells[1][1].content == theSeed
+                        && cells[2][2].content == theSeed
+                || currentRow + currentCol == 2
+                        && cells[0][2].content == theSeed
+                        && cells[1][1].content == theSeed
+                        && cells[2][0].content == theSeed);
     }
 }
