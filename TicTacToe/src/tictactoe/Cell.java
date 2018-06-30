5accf97d65cf783509ce59b5b4455f6c6bb20533
shivprogrammer <shiv.unadkat@gmail.com>
2018-06-29 23:10:04 -0700

    paint and clear methods added to cell class

--- TicTacToe/src/tictactoe/Cell.java
+++ TicTacToe/src/tictactoe/Cell.java
@@ -19,4 +19,22 @@ public class Cell {
         this.col = col;
         clear();
     }
+    
+    public void clear() {
+        content = Seed.EMPTY;
+    }
+    
+    public void paint() {
+        switch (content) {
+            case CROSS:
+                System.out.print(" X ");
+                break;
+            case NOUGHT:
+                System.out.print(" O ");
+                break;
+            case EMPTY:
+                System.out.print("   ");
+                break;
+        }
+    }
 }
