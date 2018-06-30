64e054f19759ba1af4f53eab030c0fa127b6d7ac
shivprogrammer <shiv.unadkat@gmail.com>
2018-06-30 12:55:11 -0700

    Board constructor, init  method & isDraw method added to Board class

--- TicTacToe/src/tictactoe/Board.java
+++ TicTacToe/src/tictactoe/Board.java
@@ -12,4 +12,34 @@ package tictactoe;
 public class Board {
     public static final int ROWS = 3;
     public static final int COLS = 3;
+    
+    CELL[][] cells;
+    int currentRow, currentCol;
+    
+    public Board() {
+        cells = new Cell[ROWS][COLS];
+        for (int row = 0; row < ROWS; ++row) {
+            for (int col = 0; col < COLS; ++col) {
+                cells[row][col] = new Cell(row, col);
+            }
+        }
+    }
+    
+    public void init() {
+        for (int row = 0; row < ROWS; ++row) {
+            for (int col = 0; col < COLS; ++col) {
+                cells[row][col].clear();
+            }
+        }
+    }
+    
+    public boolean isDraw() {
+        for (int row = 0; row < ROWS; ++row) {
+            for (int col = 0; col < COLS; ++col) {
+                if (cells[row][col].content == Seed.EMPTY) {
+                    return false;
+                }
+            }
+        }
+    }
 }
