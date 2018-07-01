15394fc841d458f447cfca62d39366dfa9265657
shivprogrammer <shiv.unadkat@gmail.com>
2018-06-30 23:58:54 -0700

    IVE GOT THE CONSOLE INPUTS SUCCESSFULLY WORKINGgit add .git add .git add . HAHAHSYESSSS

--- TicTacToe/src/tictactoe/Shivvy.java
+++ TicTacToe/src/tictactoe/Shivvy.java
@@ -6,7 +6,7 @@ public class Shivvy {
     static String[] gameBoard;
     static GameState currentState;
     
-//    private static Scanner in = new Scanner(System.in);
+    private static Scanner in = new Scanner(System.in);
     
     public static void clearScreen() {  
         System.out.print("\033[H\033[2J");  
@@ -17,7 +17,6 @@ public class Shivvy {
         System.out.println("Welcome to TicTacToe");
         System.out.println("This is going to be a battle of Man vs. Machine. Human, you can go first as 'X'");
         System.out.println("You can enter an integer choice based on the game board map below");
-        System.out.println("Ready to Play?");
     }
     
     public static void printBoardMap() {
@@ -46,6 +45,7 @@ public class Shivvy {
         clearScreen();
         IntroMessage();
         printBoardMap();
+        System.out.println("Ready to Play?");
         
         gameBoard = new String[9];
         
@@ -53,7 +53,14 @@ public class Shivvy {
         
         if (currentState == GameState.PLAYING) {
             clearScreen();
-            System.out.println("ITS GAME TIME BABY");
+            
+            int playerInput = in.nextInt();
+            
+            switch (playerInput) {
+                case 0:
+                    System.out.println("did it work?");
+                    break;
+            }
         }
 //        while (currentState == GameState.PLAYING) {
 //            int playerInput;
