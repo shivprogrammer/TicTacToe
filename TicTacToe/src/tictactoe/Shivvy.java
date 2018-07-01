55a8c0d1e276f0da440620dbde7f99b460fab204
shivprogrammer <shiv.unadkat@gmail.com>
2018-06-30 22:51:00 -0700

    blank game board successfully printed

--- TicTacToe/src/tictactoe/Shivvy.java
+++ TicTacToe/src/tictactoe/Shivvy.java
@@ -1,12 +1,28 @@
 package tictactoe;
 
 public class Shivvy {
+    static String[] gameBoard;
+    
+    public static void clearScreen() {  
+        System.out.print("\033[H\033[2J");  
+        System.out.flush();  
+    }  
     
     public static void IntroMessage() {
         System.out.println("Welcome to TicTacToe");
     }
     
+    static void printBoard() {
+        System.out.println("     |     |     ");
+        System.out.println("----------");
+        System.out.println("     |     |     ");
+        System.out.println("----------");
+        System.out.println("     |     |     ");
+    }
+    
     public static void main(String[] args) {
+        clearScreen();
         IntroMessage();
+        printBoard();
     }
 }
