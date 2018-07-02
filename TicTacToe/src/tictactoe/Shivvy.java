0166d1daf84b4a35ffc2aed6da0f03acb3288725
shivprogrammer <shiv.unadkat@gmail.com>
2018-07-01 22:52:28 -0700

    game is ending in a successful build upon winner :) but it's one turn late still

--- TicTacToe/src/tictactoe/Shivvy.java
+++ TicTacToe/src/tictactoe/Shivvy.java
@@ -7,13 +7,13 @@ public class Shivvy {
     public static final int EX = 1;
     public static final int OH = 2;
     
-    public static final int ROWS = 3;
-    public static final int COLS = 3;
+    public static final int TOTAL_ROWS = 3;
+    public static final int TOTAL_COLS = 3;
     public static final int MAXIMUM_MOVES = 9;
     
     public static int moveCount = 0;
     
-    public static int[][] gameBoard = new int[ROWS][COLS];
+    public static int[][] gameBoard = new int[TOTAL_ROWS][TOTAL_COLS];
     static GameState currentState;
     
     private static Scanner in = new Scanner(System.in);
@@ -44,65 +44,68 @@ public class Shivvy {
     public static void playGame() {
         initializeGame();
         
-        while (currentState == GameState.PLAYING)
+        while (currentState == GameState.PLAYING) {
             playerMove();
+            computerMove();
+        }
+    }
+    
+    public static void checkGameState(int whoseTurn, int rowChoice, int colChoice) {
+        if (isThereWinner(EX, rowChoice, colChoice)) {
+            currentState = GameState.YOU_WON;
+        } else if (moveCount == MAXIMUM_MOVES) {
+            currentState = GameState.TIE_GAME;
+        }
     }
     
     public static void initializeGame() {
         clearScreen();
-        
-        for (int row = 0; row < ROWS; ++row) {
-            for (int col = 0; col < COLS; ++col) {
+        printBlankBoard();
+        currentState = GameState.PLAYING;
+    }
+    
+    public static void printBlankBoard() {
+         for (int row = 0; row < TOTAL_ROWS; ++row) {
+            for (int col = 0; col < TOTAL_COLS; ++col) {
                 gameBoard[row][col] = EMPTY;
             }
         }
-        
-        currentState = GameState.PLAYING;
     }
         
     public static void playerMove() {
-        System.out.println("Please enter your move as two separate integers ranging from 1 to 3");
-        boolean validMove = false;
+        System.out.print("Please enter your move as 2 separate integers ranging from 1 to 3, first for x coordinate and 2nd for y coordinate: ");
         
-        do {
-            int playerRowChoice = in.nextInt() - 1;
-            int playerColChoice = in.nextInt() - 1;
-            
+        boolean validMove = false;
+        int playerRowChoice = in.nextInt() - 1;
+        int playerColChoice = in.nextInt() - 1;
+        
+        do {    
             if ((playerRowChoice <= 2 && playerRowChoice >= 0)
                  && (playerColChoice <=2 && playerColChoice >= 0)
                  && gameBoard[playerRowChoice][playerColChoice] == EMPTY) {
                     System.out.println("Successful move");
                     gameBoard[playerRowChoice][playerColChoice] = EX;
                     moveCount++;
-                    
                     printBoard();
-                    
-                    if (didExWin(playerRowChoice, playerColChoice)) {
-                        currentState = GameState.YOU_WON;
-                    }
-                    
-                    if (moveCount == MAXIMUM_MOVES) {
-                        currentState = GameState.TIE_GAME;
-                    }
-                    
                     validMove = true;
                 }
             
             else
-                System.out.println("Invalid move, please try again");
+                System.out.print("Invalid move, please try again");
             
         } while (!validMove);
         
-        computerMove();
+        checkGameState(EX, playerRowChoice, playerColChoice);
     }
         
     public static void computerMove() {
-        System.out.println("Computer, please make your move");
+        System.out.print("Computer, please make your move ");
+        
         boolean validMove = false;
+        int compRowChoice = in.nextInt() - 1;
+        int compColChoice = in.nextInt() - 1;
         
         do {
-            int compRowChoice = in.nextInt() - 1;
-            int compColChoice = in.nextInt() - 1;
 //            int compRowChoice = (int) Math.floor(Math.random() * 2);
 //            int compColChoice = (int) Math.floor(Math.random() * 2);
             
@@ -112,77 +115,70 @@ public class Shivvy {
                     moveCount++;
                     printBoard();
                     validMove = true;
-            }
-            else {
-                System.out.println("The computer fucked up");
-            }
-            
+            } else
+                System.out.print("Invalid, try again");
             
         } while (!validMove);
+        
+        checkGameState(OH, compRowChoice, compColChoice);
     }
         
-    public static boolean didExWin(int playerRowChoice, int playerColChoice) {
+    public static boolean isThereWinner(int whoseTurn, int playerRowChoice, int playerColChoice) {
         return (
-                (gameBoard[playerRowChoice][0] == EX
-                && gameBoard[playerRowChoice][1] == EX
-                && gameBoard[playerRowChoice][2] == EX)
+                (gameBoard[playerRowChoice][0] == whoseTurn
+                && gameBoard[playerRowChoice][1] == whoseTurn
+                && gameBoard[playerRowChoice][2] == whoseTurn)
              ||
-                (gameBoard[0][playerColChoice] == EX
-                && gameBoard[1][playerColChoice] == EX
-                && gameBoard[2][playerColChoice] == EX)
+                (gameBoard[0][playerColChoice] == whoseTurn
+                && gameBoard[1][playerColChoice] == whoseTurn
+                && gameBoard[2][playerColChoice] == whoseTurn)
              ||
-                (gameBoard[0][0] == EX
-                && gameBoard[1][1] == EX
-                && gameBoard[2][2] == EX)
+                (gameBoard[0][0] == whoseTurn
+                && gameBoard[1][1] == whoseTurn
+                && gameBoard[2][2] == whoseTurn)
              ||
-                (gameBoard[0][2] == EX
-                && gameBoard[1][1] == EX
-                && gameBoard[2][0] == EX)
+                (gameBoard[0][2] == whoseTurn
+                && gameBoard[1][1] == whoseTurn
+                && gameBoard[2][0] == whoseTurn)
                 );
     }
         
     public static void main(String[] args) {
         IntroMessage();
-        System.out.println("Ready to Go? Type anything to play");
+        System.out.print("Ready to Go? Type any buttons to start ");
         
         String initiateGame  = in.next();
         
         // Game is ongoing
         if (!(initiateGame == "")) {
-            System.out.println("Excellent, let us play the game");
             playGame();
             
             // Game Results
             if (currentState == GameState.YOU_WON) {
                 System.out.println("You're a winner my friend");
-            }
-            else if (currentState == GameState.TIE_GAME) {
+            } else if (currentState == GameState.TIE_GAME) {
                 System.out.println("This game has resulted in a draw");
-            }
-            else if (currentState == GameState.YOU_LOST)
+            } else if (currentState == GameState.YOU_LOST)
                 System.out.println("Well, looks like Artificial Intelligence is going to take over the world");
-            
-//        else
-//            System.out.println("Okay, have a great day");
+        }
     }
-        
-           public static void printBoard() {
-      for (int row = 0; row < ROWS; ++row) {
-         for (int col = 0; col < COLS; ++col) {
+
+    public static void printBoard() {
+      for (int row = 0; row < TOTAL_ROWS; ++row) {
+         for (int col = 0; col < TOTAL_COLS; ++col) {
             printCell(gameBoard[row][col]);
-            if (col != COLS - 1) {
+            if (col != TOTAL_COLS - 1) {
                System.out.print("|");
             }
          }
          System.out.println();
-         if (row != ROWS - 1) {
+         if (row != TOTAL_ROWS - 1) {
             System.out.println("-----------");
          }
       }
       System.out.println();
    }
  
-   /** Print a cell with the specified "content" */
    public static void printCell(int content) {
       switch (content) {
          case EMPTY:
