119b9871f3e7218c114ad4ac115834734624f9da
shivprogrammer <shiv.unadkat@gmail.com>
2018-07-01 18:30:29 -0700

    rough testing for PlayerMoves in just conceptual terms written out in comments

--- TicTacToe/src/tictactoe/Shivvy.java
+++ TicTacToe/src/tictactoe/Shivvy.java
@@ -61,13 +61,19 @@ public class Shivvy {
     }
     
     // TESTING FOR createEmptyBoard
-    // Test Name: isBoardEmptyAtBeginning() {
+    public void isBoardEmptyAtBeginning() {
         createEmptyBoard();
-        Test.assertEquals(gameBoard[0][0], EMPTY)
+        
+        Test.assertEquals(gameBoard[0][0], EMPTY);
+        Test.assertEquals(gameBoard[0][1], EMPTY);
+        Test.assertEquals(gameBoard[0][2], EMPTY);
+        Test.assertEquals(gameBoard[1][0], EMPTY);
+        Test.assertEquals(gameBoard[1][1], EMPTY);
+        Test.assertEquals(gameBoard[1][2], EMPTY);
+        Test.assertEquals(gameBoard[2][0], EMPTY);
+        Test.assertEquals(gameBoard[2][1], EMPTY);
+        Test.assertEquals(gameBoard[2][2], EMPTY);
     }
-    
-
-
     ///// END TESTING /////
     
     public static void playerMove() {
@@ -94,6 +100,13 @@ public class Shivvy {
         
         computerMove();
     }
+    
+    // TESTING FOR createEmptyBoard
+    // Nevermind, you don't want to emulate the Scanner (but I don't know how the hell I am going to run the PlayerMove method without activating the damn scanner, but at least I can manually put the moves that a player could be expected to make -- this one may be an annoyance)
+    public void legalPlayerMoves() {
+        //
+    }
+    ///// END TESTING /////
     
     public static void computerMove() {
         boolean validMove = false;
