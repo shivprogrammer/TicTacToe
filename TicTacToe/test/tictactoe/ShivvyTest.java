7b5f89b480b02fc5f4570a6ff529edb40eaa5ed8
shivprogrammer <shiv.unadkat@gmail.com>
2018-07-02 03:35:20 -0700

    first successful test for an empty gameBoard at the beginninggit add .

--- TicTacToe/test/tictactoe/ShivvyTest.java
+++ TicTacToe/test/tictactoe/ShivvyTest.java
@@ -12,36 +12,35 @@ public class ShivvyTest {
     public static final int EMPTY = 0;
     public static final int EX = 1;
     public static final int OH = 2;
-    public static int[][] drawGame = new int[3][3];
+    
+    public static int[][] tieGame = new int[3][3];
+    public static int[][] gameBoard = new int[3][3];
+    
     public static GameState currentState = GameState.PLAYING;
     
-//    @Test
-//    public void isBoardEmptyAtBeginning() {
-//        public static int[][] gameBoard = new int[3][3];
-//        
-//        Shivvy.createEmptyBoard();
-//        
-//        assertEquals(gameBoard[0][0], EMPTY);
-//        assertEquals(gameBoard[0][1], EMPTY);
-//        assertEquals(gameBoard[0][2], EMPTY);
-//        assertEquals(gameBoard[1][0], EMPTY);
-//        assertEquals(gameBoard[1][1], EMPTY);
-//        assertEquals(gameBoard[1][2], EMPTY);
-//        assertEquals(gameBoard[2][0], EMPTY);
-//        assertEquals(gameBoard[2][1], EMPTY);
-//        assertEquals(gameBoard[2][2], EMPTY);
-//    }
+    @Test
+    public void isBoardEmptyAtBeginning() {
+        assertEquals(gameBoard[0][0], EMPTY);
+        assertEquals(gameBoard[0][1], EMPTY);
+        assertEquals(gameBoard[0][2], EMPTY);
+        assertEquals(gameBoard[1][0], EMPTY);
+        assertEquals(gameBoard[1][1], EMPTY);
+        assertEquals(gameBoard[1][2], EMPTY);
+        assertEquals(gameBoard[2][0], EMPTY);
+        assertEquals(gameBoard[2][1], EMPTY);
+        assertEquals(gameBoard[2][2], EMPTY);
+    }
         
 //    @Test
 //    public void gameResultsInDraw() {
-//        drawGame[0][0] = 1;
-//        drawGame[0][1] = 2;
-//        drawGame[0][2] = 1;
-//        drawGame[1][0] = 2;
-//        drawGame[1][1] = 1;
-//        drawGame[1][2] = 2;
-//        drawGame[2][0] = 1;
-//        drawGame[2][1] = 2;
+//        tieGame[0][0] = 1;
+//        tieGame[0][1] = 2;
+//        tieGame[0][2] = 1;
+//        tieGame[1][0] = 2;
+//        tieGame[1][1] = 1;
+//        tieGame[1][2] = 2;
+//        tieGame[2][0] = 1;
+//        tieGame[2][1] = 2;
 //        
 //        // Making the last move in a game to result in a tie
 //        assertEquals(checkGameStatus(EX, 3, 3), GameState.TIE_GAME); 
